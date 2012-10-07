f3fad09faad2b84971d9fc7889a9beca9554bea9
Bryan Stearns <bryanstearns@gmail.com>
2012-10-07 14:21:46 -0700

    Configure email_spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,15 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'email_spec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include EmailSpec::Helpers
+  config.include EmailSpec::Matchers
   config.include FactoryGirl::Syntax::Methods
 
   # ## Mock Framework
