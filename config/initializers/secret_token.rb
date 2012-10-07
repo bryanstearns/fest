2db435a2c55e0876797b75f78d150a1937396596
Bryan Stearns <bryanstearns@gmail.com>
2012-10-07 12:05:25 -0700

    rails new fest -T -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fest::Application.config.secret_token = '1c8a96df00451ac40052cb177642a324cf8cded1581c93de1fa9f7d0e4202a5abec8db055966dad71aab1bdec0edd7be200ae1a65cbb2eaac5c6c3ef9d661306'
