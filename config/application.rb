800546c50a7167af340a0862686aaa7425bceea9
Bryan Stearns <bryanstearns@gmail.com>
2012-10-07 12:40:54 -0700

    Set rspec as the test framework

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,9 @@ module Fest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
