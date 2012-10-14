2efe106ad085e5d1ee989bcd3e9ce2b4ca39a182
Bryan Stearns <bryanstearns@gmail.com>
2012-10-14 15:08:48 -0700

    Validate festival date range

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -5,10 +5,18 @@ class Festival < ActiveRecord::Base
   before_validation :update_slug
 
   validates :name, :slug, presence: true, uniqueness: true
-  validates :location, :slug_group, presence: true
+  validates :location, :slug_group, :starts_on, :ends_on, presence: true
+  validate :date_range_ordering
 
 private
   def update_slug
     self.slug = "#{slug_group}_#{starts_on.strftime("%Y")}" if slug_group && starts_on
+  end
+
+  def date_range_ordering
+    return true unless starts_on && ends_on && ends_on < starts_on
+
+    errors.add(:ends_on, "cannot be before the start date")
+    false
   end
 end
