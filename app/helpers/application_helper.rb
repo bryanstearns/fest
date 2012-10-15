37b55d821626e0e54e3a7c25f7d7b4083ac5ea2b
Bryan Stearns <bryanstearns@gmail.com>
2012-10-14 19:40:03 -0700

    Add date-range helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,23 @@ module ApplicationHelper
   def current_user_is_admin?
     user_signed_in? && current_user.admin?
   end
+
+  def date_range(starts, ends)
+    return nil unless starts && ends # unset
+    return l(starts, format: :mdy) if starts == ends # it's one day
+
+    start_format, end_format = if starts.year != ends.year
+      [ :mdy, :mdy ]
+    elsif starts.month == ends.month
+      [ :md, :dy ]
+    else
+      [ :md, :mdy ]
+    end
+
+    safe_join [
+      localize(starts, format: start_format),
+      " &ndash; ".html_safe,
+      localize(ends, format: end_format)
+    ]
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   helper ApplicationHelper
+
   context "current_user_is_admin?" do
     subject { helper.current_user_is_admin? }
     context "without user logged in" do
@@ -21,6 +22,36 @@ describe ApplicationHelper do
         helper.stub(current_user: build(:user, :admin => true))
       end
       it { should be_true }
+    end
+  end
+
+  context "A formatted date range" do
+    subject { date_range(start_on, end_on) }
+    let(:start_on) { Date.parse("2015-11-20") }
+
+    context "for a single date" do
+      let(:end_on) { start_on }
+      it { should == "Nov 20, 2015" }
+    end
+
+    context "for dates within a month" do
+      let(:end_on) { start_on + 3 }
+      it { should == "Nov 20 &ndash; 23, 2015" }
+    end
+
+    context "for cross-month dates within a year" do
+      let(:end_on) { start_on + 30 }
+      it { should == "Nov 20 &ndash; Dec 20, 2015" }
+    end
+
+    context "for cross-year dates" do
+      let(:end_on) { start_on + 60 }
+      it { should == "Nov 20, 2015 &ndash; Jan 19, 2016" }
+    end
+
+    context "for unset dates" do
+      let(:end_on) { nil }
+      it { should be_nil }
     end
   end
 end
