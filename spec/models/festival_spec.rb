7fdc53c025947c28b2619f76b8bd0e2e4a5caa37
Bryan Stearns <bryanstearns@gmail.com>
2012-12-09 21:02:37 -1000

    More festival validation specs

--- spec/models/festival_spec.rb
+++ spec/models/festival_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe Festival do
-  context "validations" do
+  context "when built" do
     subject { build(:festival) }
+
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:location) }
     it { should validate_presence_of(:slug_group) }
+    it { should validate_presence_of(:starts_on) }
+    it { should validate_presence_of(:ends_on) }
+
+    it "should validate that dates are sequential" do
+      subject.should be_valid
+      subject.ends_on = subject.starts_on - 2
+      subject.should_not be_valid
+    end
+
     it "should set slug automatically" do
       subject.valid?
       subject.slug.should == "#{subject.slug_group}_#{subject.starts_on.strftime("%Y")}"
