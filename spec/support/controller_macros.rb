6615b6dc1ecd17ebdd602bfd2c9bc054a1d250b6
Bryan Stearns <bryanstearns@gmail.com>
2013-01-14 09:28:22 -0800

    in specs, note the user we're fake-signing-in-as

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,21 +2,23 @@ module ControllerMacros
   def login_admin
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in FactoryGirl.create(:confirmed_admin_user)
+      @signed_in_user = FactoryGirl.create(:confirmed_admin_user)
+      sign_in @signed_in_user
     end
   end
 
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:confirmed_user)
-      sign_in user
+      @signed_in_user = FactoryGirl.create(:confirmed_user)
+      sign_in @signed_in_user
     end
   end
 
   def logged_out
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
+      @signed_in_user = nil
       sign_out :user
     end
   end
