6080a5dfe7fe51c1d3d438d40aed13ec8bd81f7d
Bryan Stearns <bryanstearns@gmail.com>
2013-01-15 14:20:39 -0800

    Change screening date presentations

--- app/helpers/screenings_helper.rb
+++ app/helpers/screenings_helper.rb
@@ -1,6 +1,13 @@
 module ScreeningsHelper
-  def screening_times(screening)
-    "#{l screening.starts_at, format: :dmdy_hm} - " +
-        "#{l screening.ends_at, format: :hm}"
+  def screening_times(screening, options={})
+    result = time_range(screening)
+    if options[:with_date]
+      result = safe_join [
+        l(screening.starts_at.to_date, format: :mdy),
+        ' ',
+        result
+      ]
+    end
+    result
   end
 end
