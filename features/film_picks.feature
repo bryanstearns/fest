395d6c54763153d6d36ad4d1b4fda1c4f8a3f841
Bryan Stearns <bryanstearns@gmail.com>
2013-01-19 16:08:45 -0800

    Add @javascript tests for prioritizing/rating films

--- features/film_picks.feature
+++ features/film_picks.feature
@@ -6,3 +6,32 @@ Feature: Festival Film Priorities Page
     Given a festival with films and screenings
     And I visit the priorities page
     Then I should see a list of all the films
+
+  @javascript
+  Scenario: Trying to prioritize when not logged in
+    Given a festival with films and screenings
+    And I am not logged in
+    And I visit the priorities page
+    And I might get an alert
+    When I set a film priority
+    Then I should see an alert saying 'sign in to prioritize your films'
+
+  @javascript
+  Scenario: Prioritizing a film
+    Given a festival with films and screenings
+    And I am logged in
+    And I visit the priorities page
+    And I might get an alert
+    When I set a film priority
+    Then I should not see an alert
+    And the film priority should be set
+
+  @javascript
+  Scenario: Rating a film
+    Given a festival with films and screenings
+    And I am logged in
+    And I visit the priorities page
+    And I might get an alert
+    When I set a film rating
+    Then I should not see an alert
+    And the film rating should be set
--- features/step_definitions/pick_steps.rb
+++ features/step_definitions/pick_steps.rb
@@ -3,8 +3,36 @@ Given /^I visit the priorities page$/ do
   visit festival_priorities_path(@festival)
 end
 
+Given /^I might get an alert$/ do
+  page.evaluate_script('window.alert = function(message) { window.alert_message = message; return true; }')
+end
+
+When /^I set a film (priority|rating)$/ do |attr|
+  @film = @festival.films.first
+  if @user
+    pick = @festival.picks.where(user_id: @user.id, film_id: @film.id).first
+    pick.should be_nil
+  end
+  first_image = all("#film_#{@film.id}_#{attr == 'rating' ? 'stars' : 'dots'} img")[1]
+  first_image.click
+  find("#film_#{@film.id}_progress.hidden") # wait for ajax
+end
+
 Then /^I should see a list of all the films$/ do
   films_by_name = @festival.films.to_a.sort_by {|f| f.name }
   films_list = page.find("table#picks")
   films_list.should have_content(films_by_name.first.name)
+end
+
+Then /^I should see an alert saying '([^\']+)'$/ do |msg|
+  page.evaluate_script('window.alert_message').should match(Regexp.new(msg))
+end
+
+Then /^I should not see an alert$/ do
+  page.evaluate_script('window.alert_message').should be_nil
+end
+
+Given /^the film (priority|rating) should be set$/ do |attr|
+  pick = @festival.picks.where(user_id: @user.id, film_id: @film.id).first
+  pick.send(attr).should == 1
 end
