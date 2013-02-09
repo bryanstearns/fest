cd0787cf71d5a85bf62d11c85a149cea8b6968fb
Bryan Stearns <bryanstearns@gmail.com>
2013-02-09 09:19:07 -0800

    Fix another case where press screenings showed up & shouldn't've

--- app/controllers/screenings_controller.rb
+++ app/controllers/screenings_controller.rb
@@ -7,9 +7,10 @@ class ScreeningsController < ApplicationController
     @screening = Screening.includes(:festival, :film).find(params[:id])
     @festival = @screening.festival
     @film = @screening.film
-    @other_screenings = @film.screenings.includes(:venue).to_a - [@screening]
     @subscription = current_user.subscription_for(@festival) if user_signed_in?
     @show_press = @subscription.try(:show_press)
+    @other_screenings = @film.screenings.with_press(@show_press)\
+                             .includes(:venue).to_a - [@screening]
     respond_with(@screening)
   end
 end
