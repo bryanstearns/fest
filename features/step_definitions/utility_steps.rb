2c06130d0761e4c6e4acfb99503f4a649ef5e9f2
Bryan Stearns <bryanstearns@gmail.com>
2013-02-11 16:57:57 -0800

    Rework utility step to support relations

--- features/step_definitions/utility_steps.rb
+++ features/step_definitions/utility_steps.rb
@@ -1,9 +1,19 @@
-Given /^a (\S+)( with (.*))?$/ do |model, with_stuff, stuff|
-  # Given a hammer with nails -> create(:hammer, :with_nails)
+Given /^a (\S+)(.*)?$/ do |model, stuff|
+  # Given a hammer with nails for the toolbox -> create(:hammer, :with_nails, toolbox: @toolbox)
   # Given a fish with 2 bicycles -> create(:fish, :with_bicycles, bicycle_count: 2)
   traits = []
   options = {}
-  stuff.split(",").each do |trait|
+
+  raw_traits = ''
+  raw_relations = ''
+
+  if stuff =~ /with (.*)/ # has 'with', might have 'for the'
+    raw_traits, raw_relations = $1.strip.split(' for the ')
+  elsif stuff =~ /for the (.*)/ # might have 'for the'
+    raw_relations = $1.strip.split(', ')
+  end
+
+  raw_traits.split(",").each do |trait|
     # "things" | "1 thing"
     traits << if trait =~ /^(\d+)\s+(.*)$/
       name = $2.strip.gsub(' ', '_')
@@ -12,7 +22,11 @@ Given /^a (\S+)( with (.*))?$/ do |model, with_stuff, stuff|
     else
       "with_#{trait.strip.pluralize.gsub(' ', '_')}"
     end.to_sym
-  end if stuff
+  end if raw_traits
+
+  raw_relations.split(',').each do |relation|
+    options[relation.to_sym] = instance_variable_get("@#{relation}")
+  end if raw_relations
 
   #traitdesc = ", " + traits.map(&:inspect).join(", ") unless traits.empty?
   #optdesc = ", " + options.map{|k,v| "#{k}: #{v.inspect}" }.join(", ") unless options.empty?
