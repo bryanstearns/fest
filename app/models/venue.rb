cf1fe53f450f16fafce57827ea18efc8df86a36c
Bryan Stearns <bryanstearns@gmail.com>
2013-12-26 18:36:49 -0800

    Add Venue.screenings relation

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,7 @@
 class Venue < ActiveRecord::Base
   belongs_to :location, touch: true
+  has_many :screenings
+
   attr_accessible :abbreviation, :name
 
   validates :abbreviation, :location_id, :name, presence: true
