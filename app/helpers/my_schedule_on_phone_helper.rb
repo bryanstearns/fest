d2654b95f30f23b23656ee646e1cb726733cd861
Bryan Stearns <bryanstearns@gmail.com>
2014-01-17 20:06:51 -0800

    Small-screen-view changes
    - mention it in the instructions
    - make the modals read-only (no unschedule)
    - only show this view if the user has selected screenings

--- app/helpers/my_schedule_on_phone_helper.rb
+++ app/helpers/my_schedule_on_phone_helper.rb
@@ -1,6 +1,6 @@
 module MyScheduleOnPhoneHelper
   def my_schedule_on_phone?
-    user_signed_in? ? true : nil
+    (user_signed_in? && current_user.has_screenings_for?(@festival)) ? true : nil
   end
 
   def my_schedule_visible_on_phone
