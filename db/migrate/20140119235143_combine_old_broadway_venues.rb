87c822497e27a42e50182d711d8a22f4511c188d
Bryan Stearns <bryanstearns@gmail.com>
2014-01-19 17:26:42 -0800

    clean up duplicate location & venues

---
+++ db/migrate/20140119235143_combine_old_broadway_venues.rb
@@ -0,0 +1,22 @@
+class CombineOldBroadwayVenues < ActiveRecord::Migration
+  def up
+    old_location = Location.where(place: "Portland, Oregon", name: "Regal Broadway").first
+    if old_location
+      new_location = Location.where(place: "Portland, Oregon", name: "Broadway").first
+
+      old_location.venues.each do |old_venue|
+        new_venue = new_location.venues.where(abbreviation: old_venue.abbreviation).first
+        old_venue.screenings.each do |screening|
+          screening.update_attributes(venue_id: new_venue.id)
+        end
+      end
+
+      old_location.festivals.each do |festival|
+        festival.locations.destroy(old_location)
+        festival.locations << new_location
+      end
+
+      old_location.destroy
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140119184600) do
+ActiveRecord::Schema.define(:version => 20140119235143) do
 
   create_table "activity", :force => true do |t|
     t.string   "name"
