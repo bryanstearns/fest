4666a5f2fd320db70a26d105a713ca0e625f51c5
Bryan Stearns <bryanstearns@gmail.com>
2014-02-01 18:52:43 -0800

    Tweak unselect menu wording & ordering for clarity

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -14,19 +14,17 @@ module SubscriptionsHelper
   end
 
   def unselect_menu_choices(picks)
-    has_future_screenings = picks.any? {|p| p.screening.try(:future?) }
-    has_future_manual = has_future_screenings && picks.any?(&:auto)
-
     choices = []
-
     future_screening_picks = picks.select {|p| p.screening.try(:future?) }
     if future_screening_picks.present?
       choices << ['Unselect all the future screenings', 'future']
       if future_screening_picks.any?(&:auto)
         choices << ['Unselect just the future automatically-scheduled screenings - leave the manually-picked ones', 'auto']
       end
+      choices << ['Unselect all of them', 'all']
+    else
+      choices << ['Unselect them', 'all']
     end
-    choices << ['Unselect all of them', 'all']
     choices << ['Leave them selected; just fill in around them', 'none']
     choices
   end
