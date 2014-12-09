505a8976b00aeffeb0aba57be4c676656466d3bb
Bryan Stearns <bryanstearns@gmail.com>
2014-12-09 08:24:07 -1000

    Raise on unpermitted parameters in development

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Raise to help track down unpermitted parameters
+  config.action_controller.action_on_unpermitted_parameters = :raise
+
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
