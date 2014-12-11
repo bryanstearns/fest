67a723ef9ee545a09a0afd1511fff2e1ad2234f1
Bryan Stearns <bryanstearns@gmail.com>
2014-12-10 19:13:58 -1000

    Fix phone view

--- app/helpers/my_schedule_on_phone_helper.rb
+++ app/helpers/my_schedule_on_phone_helper.rb
@@ -1,13 +1,13 @@
 module MyScheduleOnPhoneHelper
   def my_schedule_on_phone?
-    (user_signed_in? && current_user.has_screenings_for?(@festival)) ? true : nil
+    user_signed_in? && current_user.has_screenings_for?(@festival)
   end
 
   def my_schedule_visible_on_phone
-    my_schedule_on_phone? && ' visible-xs'
+    ' visible-xs' if my_schedule_on_phone?
   end
 
   def my_schedule_hidden_on_phone
-    my_schedule_on_phone? && ' hidden-xs'
+    ' hidden-xs' if my_schedule_on_phone?
   end
 end
