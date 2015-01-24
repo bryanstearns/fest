92d54cae0f08aea67b7fae0845991efd36e86e99
Bryan Stearns <bryanstearns@gmail.com>
2015-01-24 15:25:31 -0800

    Externalize and commit new relic config
    Include user email as an NR parameter

--- docker_tools_config.rb
+++ docker_tools_config.rb
@@ -5,13 +5,15 @@ when 'prod'
       "RAILS_ENV" => "production",
       "REDIS_URL" => "redis://docker0/1",
       "SECRET_KEY_BASE" => secret("fest_prod/secret_key_base"),
-      "SMTP_PASSWORD" => secret("fest_prod/festfan_smtp")
+      "SMTP_PASSWORD" => secret("fest_prod/festfan_smtp"),
+      "NEWRELIC_LICENSE_KEY" => secret("fest_prod/newrelic_license_key")
   port 80 => 8111
 when 'staging'
   env "DOMAIN_NAMES" => "staging.festivalfanatic.com www.staging.festivalfanatic.com",
       "RAILS_ENV" => "staging",
       "REDIS_URL" => "redis://docker0/2",
-      "SECRET_KEY_BASE" => secret("fest_staging/secret_key_base")
+      "SECRET_KEY_BASE" => secret("fest_staging/secret_key_base"),
+      "NEWRELIC_LICENSE_KEY" => secret("fest_prod/newrelic_license_key") # staging same as prod
   port 80 => 8112
 else
   abort "Invalid fest environment: #{environment.inspect}"
