1f38e08c6750f52055b39da00ee5251d58305fd1
Bryan Stearns <bryanstearns@gmail.com>
2016-11-20 17:17:21 -0800

    Update to newer airbrake gem configuration

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,6 @@
 Airbrake.configure do |config|
-  config.api_key = '48f876c18cf0ef128d95ef7a6f59800f'
+  config.project_key = '48f876c18cf0ef128d95ef7a6f59800f'
+  config.project_id = 17719
+  config.environment = Rails.env
+  config.ignore_environments = %w(development test)
 end
