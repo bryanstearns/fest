959b6396a8b9d89d1a2d0a1ffd2c5f7f546815fd
Bryan Stearns <bryanstearns@gmail.com>
2016-11-24 14:24:57 -0800

    Fix cold redeploys

--- docker_tools_config.rb
+++ docker_tools_config.rb
@@ -20,6 +20,8 @@ else
 end
 
 setup postgres: [
+    "drop database if exists fest_#{environment}",
+    "drop role fest_#{environment}",
     "create role fest_#{environment} with superuser login createdb password 'fest_#{environment}'",
     "create database fest_#{environment};",
     "grant all privileges on database fest_#{environment} to fest_#{environment};"
