409eb1b53af5c0b006a1ad288b848660f027587d
Bryan Stearns <bryanstearns@gmail.com>
2018-12-11 14:34:14 -0600

    Start Rails 5.0 update
    Remove Gemfile.lock
    Set rails version to 5.0
    Update source
    Remove axlsx version for now
    Change to sassc-rails
    Replace quiet_assets with config
    Bundle install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Suppress output of asset requests
+  config.assets.quiet = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
